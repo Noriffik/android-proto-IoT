32ad3f1608c8545d685b602bff43332c74c99c09
bp <bernard.pletikosa@gmail.com>
2016-04-26 17:59:25 +0200

    Extract UI and notification utils in separate classes.

---
+++ app/src/main/java/io/relayr/iotsmartphone/ui/utils/NotificationsUtil.java
@@ -0,0 +1,68 @@
+package io.relayr.iotsmartphone.ui.utils;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+import io.relayr.iotsmartphone.R;
+import io.relayr.iotsmartphone.helper.DemandIntentReceiver;
+import io.relayr.iotsmartphone.storage.Constants;
+import io.relayr.iotsmartphone.ui.MainTabActivity;
+
+public class NotificationsUtil {
+
+    public static void hideNotification(Context context, int id) {
+        final NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
+        managerCompat.cancel(id);
+    }
+
+    public static void showNotification(Context context, int extra) {
+        switch (extra) {
+            case Constants.NOTIF_VIB:
+                showNotification(context, R.string.notif_vib, R.string.notif_vib_off, Constants.NOTIF_VIB);
+                break;
+            case Constants.NOTIF_SOUND:
+                showNotification(context, R.string.notif_music, R.string.notif_music_off, Constants.NOTIF_SOUND);
+                break;
+            case Constants.NOTIF_FLASH:
+                showNotification(context, R.string.notif_flash, R.string.notif_flash_off, Constants.NOTIF_FLASH);
+                break;
+        }
+    }
+
+    public static void showNotification(Context context, int titleId, int textId, int notificationId) {
+        Intent demandIntent = new Intent(context, DemandIntentReceiver.class)
+                .putExtra(DemandIntentReceiver.EXTRA_MESSAGE, notificationId)
+                .setAction(DemandIntentReceiver.ACTION_DEMAND);
+
+        PendingIntent demandPendingIntent = PendingIntent.getBroadcast(context, 0, demandIntent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+
+        NotificationCompat.Action action = new NotificationCompat.Action.Builder(R.mipmap.logo,
+                context.getString(textId), demandPendingIntent).build();
+
+        showNotification(context, action, titleId, textId, notificationId);
+    }
+
+    public static void showNotification(Context context, NotificationCompat.Action action, int titleId, int textId, int notificationId) {
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.notification)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setContentTitle(context.getString(titleId))
+                .setContentText(context.getString(textId));
+
+        if (UiUtil.isWearableConnected((MainTabActivity) context)) {
+            Bitmap bg = BitmapFactory.decodeResource(context.getResources(), R.color.primary);
+            builder.extend(new NotificationCompat.WearableExtender().addAction(action).setBackground(bg));
+        }
+        builder.addAction(action);
+
+        final NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
+        managerCompat.notify(notificationId, builder.build());
+    }
+}
---
+++ app/src/main/java/io/relayr/iotsmartphone/ui/utils/UiUtil.java
@@ -0,0 +1,99 @@
+package io.relayr.iotsmartphone.ui.utils;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.relayr.android.RelayrSdk;
+import io.relayr.iotsmartphone.R;
+
+public class UiUtil {
+
+    private static final String WEAR_APP = "com.google.android.wearable.app";
+    private static final Map<String, String> sNameMap = new HashMap<>();
+    private static final Map<String, String> sUnitMap = new HashMap<>();
+
+    public static boolean isWearableConnected(Activity activity) {
+        try {
+            activity.getPackageManager().getPackageInfo(WEAR_APP, PackageManager.GET_META_DATA);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    public static boolean isCloudConnected() {
+        return RelayrSdk.isUserLoggedIn();
+    }
+
+    public static void showSnackBar(Activity activity, int stringId) {
+        if (activity == null) return;
+        final View view = activity.findViewById(android.R.id.content);
+        if (view == null) return;
+        Snackbar.make(view, activity.getString(stringId), Snackbar.LENGTH_SHORT).show();
+    }
+
+    public static void openDashboard(Context context) {
+        String packageName = "io.relayr.wunderbar";
+        PackageManager manager = context.getPackageManager();
+        Intent startApp = manager.getLaunchIntentForPackage(packageName);
+        if (startApp != null) {
+            startApp.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startApp.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            context.startActivity(startApp);
+            return;
+        }
+
+        String urlString = "https://developer.relayr.io/";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlString));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setPackage("com.android.chrome");
+        try {
+            context.startActivity(intent);
+        } catch (ActivityNotFoundException ex) {
+            intent.setPackage(null);
+            context.startActivity(intent);
+        }
+    }
+
+    public static String getNameForMeaning(Context context, String meaning) {
+        if (sNameMap.isEmpty()) {
+            sNameMap.put("acceleration", context.getString(R.string.reading_title_acceleration));
+            sNameMap.put("angularSpeed", context.getString(R.string.reading_title_gyro));
+            sNameMap.put("batteryLevel", context.getString(R.string.reading_title_battery));
+            sNameMap.put("luminosity", context.getString(R.string.reading_title_light));
+            sNameMap.put("location", context.getString(R.string.reading_title_location));
+            sNameMap.put("rssi", context.getString(R.string.reading_title_rssi));
+            sNameMap.put("touch", context.getString(R.string.reading_title_touch));
+        }
+        return sNameMap.get(meaning);
+    }
+
+    public static String getUnitForMeaning(Context context, String meaning) {
+        if (sUnitMap.isEmpty()) {
+            sUnitMap.put("acceleration", context.getString(R.string.reading_unit_acceleration));
+            sUnitMap.put("angularSpeed", context.getString(R.string.reading_unit_acceleration));
+            sUnitMap.put("batteryLevel", context.getString(R.string.reading_unit_battery));
+            sUnitMap.put("luminosity", context.getString(R.string.reading_unit_light));
+            sUnitMap.put("rssi", context.getString(R.string.reading_unit_rssi));
+            sUnitMap.put("touch", context.getString(R.string.reading_unit_empty));
+            sUnitMap.put("location", context.getString(R.string.reading_unit_empty));
+        }
+        return sUnitMap.get(meaning);
+    }
+
+    public static void hideKeyboard(Context context, View element) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(element.getWindowToken(), InputMethodManager.RESULT_UNCHANGED_SHOWN);
+    }
+}
